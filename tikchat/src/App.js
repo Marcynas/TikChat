643d978b2c4763cf142949db490aa7eb1461c204
MartynasTv <73237433+Marcynas@users.noreply.github.com>
2022-02-28 21:26:10 +0200

    commit on 'main'

--- tikchat/src/App.js
+++ tikchat/src/App.js
@@ -5,21 +5,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
+
+        <button onclick="LoginMenu()">
+          <text>LOG-IN</text>
+        </button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Log in screen
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
+}
+function LoginMenu(){
+    
 }
 
 export default App;
