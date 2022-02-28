b228932290e677ff222c5d949fa3ffc91925fbee
MartynasTv <73237433+Marcynas@users.noreply.github.com>
2022-02-28 21:40:30 +0200

    commint on 'main'

--- tikchat/src/App.js
+++ tikchat/src/App.js
@@ -1,19 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+import { useAuthState } from 'react-firebase-hooks/auth' 
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC9HQMNAOqVuh5MRKBSxYd5_nHEV21f4_Y",
+  authDomain: "tikchatweb.firebaseapp.com",
+  projectId: "tikchatweb",
+  storageBucket: "tikchatweb.appspot.com",
+  messagingSenderId: "691401485298",
+  appId: "1:691401485298:web:ed0959edd71842fd80996b",
+  measurementId: "G-Y3B85QNHE9"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        <img src={logo} className="App-logo" alt="logo" />
 
-        <button onclick="LoginMenu()">
-          <text>LOG-IN</text>
-        </button>
-        <p>
-          Log in screen
-        </p>
       </header>
     </div>
   );
