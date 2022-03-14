ac752353efe79ddd763d7a205d6437644ac5f787
MartynasTv <73237433+Marcynas@users.noreply.github.com>
2022-03-14 15:11:56 +0200

    main

--- tikchat/src/App.js
+++ tikchat/src/App.js
@@ -88,7 +88,7 @@ function ChatRoom() {
   const dummy = useRef();
 
   const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(25);
+  const query = messagesRef.orderBy('createdAt');
 
   const [messages] = useCollectionData(query, { idField: 'id' });
 
