94255fb120818e793610b474be7c6c4c1b8cb92a
MartynasTv <73237433+Marcynas@users.noreply.github.com>
2022-03-27 19:55:57 +0300

    main

--- tikchat/src/App.js
+++ tikchat/src/App.js
@@ -7,6 +7,9 @@ import 'firebase/compat/firestore';
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { useCollectionData } from 'react-firebase-hooks/firestore'
 import * as Icon from 'react-bootstrap-icons';
+import { getDatabase, ref, push, set, child, get } from "firebase/database";
+import { Reddit } from 'react-bootstrap-icons';
+
 
 
 //---------------------------------
@@ -17,10 +20,12 @@ firebase.initializeApp({
   storageBucket: "tikchatweb.appspot.com",
   messagingSenderId: "691401485298",
   appId: "1:691401485298:web:ed0959edd71842fd80996b",
-  measurementId: "G-Y3B85QNHE9"
+  measurementId: "G-Y3B85QNHE9",
+  databaseURL: "https://tikchatweb-default-rtdb.europe-west1.firebasedatabase.app/"
 })
 const auth = firebase.auth();
 const firestore = firebase.firestore();
+const db = getDatabase();
 //---------------------------------
 
 //Pagrindine funkcija
@@ -33,17 +38,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div>
-          {auth.currentUser && (<button className='Hbtn fr' onClick={() => setFriendsHidden(s => !s)}>Friends <Icon.PeopleFill/></button>)}
+          {auth.currentUser && (<button className='Hbtn fr' onClick={() => setFriendsHidden(s => !s)}>Friends <Icon.PeopleFill /></button>)}
           {user ? !isFriendsHidden ? ' Friends ' : ' Chat room ' : ' Sign in '}
           <SignOut />
         </div>
       </header>
       <section>
-        {user ? !isFriendsHidden ? <Friends />  : <ChatRoom /> : <SignIn />}
+        {user ? !isFriendsHidden ? <Friends /> : <ChatRoom /> : <SignIn />}
       </section>
     </div>
   );
 }
+
 
 //Prisijungimo langas
 function SignIn() {
@@ -58,9 +64,9 @@ function SignIn() {
   const signInWithGithub = () => {
     const provider = new firebase.auth.GithubAuthProvider();
     auth.signInWithPopup(provider);
+
   }
   //---------------------------------
-
   //Grazina logotipa ir du mygtukus prisijungimui
   return (
     <section>
@@ -68,17 +74,28 @@ function SignIn() {
       <img alt='' className="App-logo" src={logo} />
       <>Sign in using</>
       <div>
-        <button className="Login-btn" onClick={signInWithGoogle}>Google <Icon.Google/></button>
-        <button className="Login-btn" onClick={signInWithGithub}>Github <Icon.Github/></button>
+        <button className="Login-btn" onClick={signInWithGoogle}>Google <Icon.Google /></button>
+        <button className="Login-btn" onClick={signInWithGithub}>Github <Icon.Github /></button>
       </div>
     </section>
   );
 }
+function writeUserData(userId, name, email, photoURL) {
+  const usersRef = firestore.collection('users').doc(userId);
+  usersRef.set({
+    userId,
+    name,
+    email,
+    photoURL,
+    createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+  })
+}
+
 
 //Atsijungimo mygtukas
 function SignOut() {
   return auth.currentUser && (
-    <button className='Hbtn out' onClick={() => auth.signOut()}>Sign Out <Icon.BoxArrowRight/></button>
+    <button className='Hbtn out' onClick={() => auth.signOut()}>Sign Out <Icon.BoxArrowRight /></button>
   )
 }
 
@@ -89,6 +106,8 @@ function ChatRoom() {
   const query = messagesRef.orderBy('createdAt');
   const [messages] = useCollectionData(query, { idField: 'id' });
   const [formValue, setFormValue] = useState('');
+
+  writeUserData(auth.currentUser.uid, auth.currentUser.displayName, auth.currentUser.email, auth.currentUser.photoURL);
 
   const sendMessage = async (e) => {
     e.preventDefault();
@@ -105,6 +124,7 @@ function ChatRoom() {
     dummy.current.scrollIntoView({ behavior: 'smooth' });
   }
 
+
   return (<>
     <div>
       <div>
@@ -114,42 +134,133 @@ function ChatRoom() {
         </main>
         <form onSubmit={sendMessage}>
           <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="Message text..." required maxlength="59" />
-          <button className='Hbtn snd' type="submit" disabled={!formValue}><Icon.Send/></button>
+          <button className='Hbtn snd' type="submit" disabled={!formValue}><Icon.Send /></button>
         </form>
       </div>
     </div>
   </>)
 }
 
-
-
-//Draugu langas
-function Friends() {
-  return (
-    <>
-      <main>
-        Draugai
-      </main>
-    </>
-  )
-}
-
 //Zinutes burbuliukas
 function ChatMessage(props) {
   const { text, uid, photoURL, displayName } = props.message;
   const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
-
   return (<div className='messageBox'>
     <a className={`message ${messageClass}`}>{displayName ? displayName : "GithubUser"}</a>
     <div className={`message ${messageClass}`}>
-      <img className='Profile-img name' src={
+      <img onClick={() => AddFriend(auth.currentUser.uid, uid)} className='Profile-img name' src={
         photoURL ? photoURL : "https://icon-library.com/images/anonymous-icon/anonymous-icon-0.jpg"} />
       <p>{text}</p>
     </div>
   </div>
-
-
   )
 }
+
+//Draugu langas
+function Friends() {
+  const friendsRef = firestore.collection('friends' + auth.currentUser.uid);
+  const query = friendsRef.orderBy('createdAt');
+  const [friends] = useCollectionData(query, { idField: 'id' });
+  return (<>
+    <div>
+      <div>
+      <main>
+          {friends && friends.map(fr => <FriendLine key={fr.id} friend={fr} />)}
+        </main>
+      </div>
+    </div>
+  </>)
+}
+
+function FriendLine(props) {
+  const { userIdFR } = props.friend;
+
+  console.log(  getData(userIdFR).then((name)=>{
+    return name
+  }
+  ))
+  return (<div className='messageBox'>
+  <main>
+  {userIdFR}
+  </main>
+</div>
+);
+};
+
+const getData = async (userIdFR) =>{
+  try{
+  return  await (firestore.collection('users').doc(userIdFR).get()).then((a) =>{
+    a.then((doc) => {
+      return doc.data().name
+    }
+    )
+  })
+  }
+  catch(err){
+    return "name";
+  }
+}
+
+
+
+
+// usersRef.withConverter(userConverter).get().then((doc) => {
+//   if (doc.exists){
+//     // Convert to City object
+//     var city = doc.data();
+//     // Use a City instance method
+//     return (<div className='messageBox'>
+//     <a>{userIdFR}</a>
+//   </div>
+//   )
+//   } else {
+//     console.log("No such document!");
+//   }}).catch((error) => {
+//     console.log("Error getting document:", error);
+//   });
+
+
+
+
+
+const AddFriend = (userId, userIdFR) => {
+  const friendsRef = firestore.collection('friends' + userId).doc(userIdFR);
+  friendsRef.set({
+    userIdFR,
+    createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+  })
+}
+
+
+
+// class User {
+//   constructor (userId, name, email, photoURL) {
+//       this.userId = userId;
+//       this.name = name;
+//       this.email = email;
+//       this.photoURL = photoURL;
+//   }
+//   getName(){
+//     return this.name;
+//   }
+// }
+
+// // Firestore data converter
+// var userConverter = {
+//   toFirestore: function(user) {
+//       return {
+//           userId: user.userId,
+//           name: user.name,
+//           email:user.email,
+//           photoURL: user.photoURL
+
+//           };
+//   },
+//   fromFirestore: function(snapshot, options){
+//       const data = snapshot.data(options);
+//       return new User(data.userId, data.name, data.email, data.photoURL);
+//   }
+// };
+
 
 export default App;
