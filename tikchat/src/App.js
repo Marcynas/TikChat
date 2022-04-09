3768486f0b128c99721e9ed97e2728ecc5effe38
MartynasTv <73237433+Marcynas@users.noreply.github.com>
2022-04-09 13:27:28 +0300

    main

--- tikchat/src/App.js
+++ tikchat/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {makePhoto,generateName} from './generator.js';
+import { makePhoto, generateName } from './generator.js';
 import logo from './logo.svg';
 import Linkify from 'react-linkify';
 import React, { useRef, useState } from "react";
@@ -11,7 +11,7 @@ import { useCollectionData } from 'react-firebase-hooks/firestore'
 import * as Icon from 'react-bootstrap-icons';
 import { updateProfile } from 'firebase/auth';
 import { onSnapshot, enableIndexedDbPersistence } from 'firebase/firestore';
-import 'firebase/compat/storage'; 
+import 'firebase/compat/storage';
 
 //-----------------------------------------------------------------
 firebase.initializeApp({
@@ -36,15 +36,15 @@ var storage = firebase.storage();
 
 enableIndexedDbPersistence(firestore)
   .catch((err) => {
-      if (err.code === 'failed-precondition') {
-          // Multiple tabs open, persistence can only be enabled
-          // in one tab at a a time.
-          // ...
-      } else if (err.code === 'unimplemented') {
-          // The current browser does not support all of the
-          // features required to enable persistence
-          // ...
-      }
+    if (err.code === 'failed-precondition') {
+      // Multiple tabs open, persistence can only be enabled
+      // in one tab at a a time.
+      // ...
+    } else if (err.code === 'unimplemented') {
+      // The current browser does not support all of the
+      // features required to enable persistence
+      // ...
+    }
   });
 // Subsequent queries will use persistence, if it was enabled successfully
 
@@ -56,36 +56,34 @@ function App() {
   const [user] = useAuthState(auth);
   const [screen, setScreen] = useState('ChatRoom');
   const handleClick = (screenState) => {
-    setScreen(screenState)} //pakeicia ekrana
-  
+    setScreen(screenState)
+  } //pakeicia ekrana
+
   return (
     <div className="App">
       <header className="App-header">
-        <div> 
+        <div>
           {user ? (() => {
-        switch (screen) {
-          case 'ChatRoom':
-            return <><button className='Hbtn fr' onClick={() => {handleClick('Friends')}}>Friends <Icon.PeopleFill /></button>   Chat room </>;
-          case 'Friends':
-            return <><button className='Hbtn fr' onClick={() => {handleClick('ChatRoom')}}>Back <Icon.ArrowReturnLeft /></button>   Friends </>
-          default: 
-            return null
-        }
-      })() : '  Sign in  '}
+            switch (screen) {
+              case 'ChatRoom':
+                return <><button className='Hbtn fr' onClick={() => { handleClick('Friends') }}>Friends <Icon.PeopleFill /></button>   Chat room </>;
+              case 'Friends':
+                return <><button className='Hbtn fr' onClick={() => { handleClick('ChatRoom') }}>Back <Icon.ArrowReturnLeft /></button>   Friends </>
+              default:
+                return null
+            }
+          })() : '  Sign in  '}
           <SignOut />
         </div>
       </header>
       <section>
-        {user ? (() => {
-        switch (screen) {
-          case 'ChatRoom':
-            return <ChatRoom handleClick={handleClick} />
-          case 'Friends':
-            return <Friends handleClick={handleClick} />
-          default:
-            return null
-        }
-      })() : <SignIn />}
+          {user && (
+            <>
+              {screen === 'ChatRoom' && <ChatRoom handleClick={handleClick} />}
+              {screen === 'Friends' && <Friends handleClick={handleClick} />}
+            </>
+          )}
+          {!user && <SignIn />}
       </section>
     </div>
   );
@@ -113,15 +111,15 @@ function SignIn() {
   }
   //Prisijungimas su Email
   const signInWithEmail = () => {
-  ui.start('#firebaseui-auth-container', {
-    signInOptions: [
-      {
-        signInFlow: 'popup',
-        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        requireDisplayName: true,
-      }
-    ]
-  });
+    ui.start('#firebaseui-auth-container', {
+      signInOptions: [
+        {
+          signInFlow: 'popup',
+          provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+          requireDisplayName: true,
+        }
+      ]
+    });
   }
   const [isSignInHidden, setSignInHidden] = useState(true);
   //-----------------------------------------------------------------
@@ -132,7 +130,7 @@ function SignIn() {
       <>Sign in using</>
       <div className={`hidden${isSignInHidden}`} id="firebaseui-auth-container"></div>
       <div>
-        <button className="Login-btn" onClick={() => {setSignInHidden(s => !s); signInWithEmail();}}>Email <Icon.EnvelopeFill /></button>
+        <button className="Login-btn" onClick={() => { setSignInHidden(s => !s); signInWithEmail(); }}>Email <Icon.EnvelopeFill /></button>
         <button className="Login-btn" onClick={signInAnonymous}>Guest <Icon.PersonFill /></button>
       </div>
       <div>
@@ -156,20 +154,20 @@ function SignOut() {
 function ChatRoom() {
   const dummy = useRef();
   const messagesRef = firestore.collection(pokalbis);
-  onSnapshot(messagesRef,()=>{
+  onSnapshot(messagesRef, () => {
     dummy.current.scrollIntoView({ behavior: 'smooth' })
   })
   const query = messagesRef.orderBy('createdAt');
   const [messages] = useCollectionData(query, { idField: 'id' });
   const [formValue, setFormValue] = useState('');
   if (auth.currentUser.photoURL == null)
-  updateProfile(auth.currentUser, {
-    photoURL: makePhoto(10)
-  });
+    updateProfile(auth.currentUser, {
+      photoURL: makePhoto(10)
+    });
   if (auth.currentUser.displayName == null)
-  updateProfile(auth.currentUser, {
-    displayName: generateName()
-  });
+    updateProfile(auth.currentUser, {
+      displayName: generateName()
+    });
 
   const sendMessage = async (e) => {
     e.preventDefault();
@@ -184,8 +182,6 @@ function ChatRoom() {
     setFormValue('');
     dummy.current.scrollIntoView({ behavior: 'smooth' });
   }
-
-
 
   //upload image
   const uploadImage = async (e) => {
@@ -212,9 +208,9 @@ function ChatRoom() {
     <div>
       <div>
         <main className='main-chat'>
-          <br/>
-          <b><Icon.ChatSquareDots size={160}/></b><br/>
-          <b>Say hello! 👋</b>
+          <br />
+          <b><Icon.ChatSquareDots size={160} /></b><br />
+          <b>Say hello! 👋</b><br />
           {messages && messages.map(msg => <ChatMessage message={msg} key={msg.id} />)}
           <span ref={dummy}></span>
         </main>
@@ -226,7 +222,7 @@ function ChatRoom() {
           <input type="file" accept="image/*" id="upload" hidden onChange={uploadImage} />
           <label className='Hbtn upl formMSGbutton' htmlFor="upload"><Icon.Upload /></label>
         </form>
-        
+
       </div>
     </div>
   </>)
@@ -248,7 +244,7 @@ function ChatMessage(props) {
   return (<li className='messageBox'>
     <b className={`message ${messageClass} wrap`}>{displayName ? displayName : "UserWithoutName"}</b>
     <div className={`message ${messageClass}`}>
-      <img alt=':(' onClick={() => AddFriend(auth.currentUser.uid, uid, displayName,photoURL) & setFriend('myFriend') } className={`Profile-img ${isFriends} name`} src={
+      <img alt=':(' onClick={() => AddFriend(auth.currentUser.uid, uid, displayName, photoURL) & setFriend('myFriend')} className={`Profile-img ${isFriends} name`} src={
         photoURL ? photoURL : "https://icon-library.com/images/anonymous-icon/anonymous-icon-0.jpg"} />
       <p>
         {text.includes('/images%') ? <img alt=':(' className='message-img' src={text} /> : <Linkify properties={{ target: '_blank', style: { color: 'blue' } }}>{text}</Linkify>}
@@ -269,8 +265,8 @@ function Friends(props) {
     <div>
       <div>
         <main className='FriendBoxBox'>
-        <b className='FriendBox' onClick={() => {pokalbis="messages"; props.handleClick('ChatRoom')} }>Public</b>
-        {friends && friends.map(fr => <FriendBox key={fr.id} friend={fr} handleClick={props.handleClick} />)}
+          <b className='FriendBox' onClick={() => { pokalbis = "messages"; props.handleClick('ChatRoom') }}>Public</b>
+          {friends && friends.map(fr => <FriendBox key={fr.id} friend={fr} handleClick={props.handleClick} />)}
         </main>
       </div>
     </div>
@@ -279,18 +275,20 @@ function Friends(props) {
 
 //Draugo burbuliukas
 function FriendBox(props) {
-  const { uid, displayName ,photoURL } = props.friend;
+  const { uid, displayName, photoURL } = props.friend;
   return (
     <div>
-          <div className='FriendBox' onClick={() => {PakeistiPokalbi(uid); props.handleClick('ChatRoom')}}>
-    <img alt=':(' className='Profile-imgFR name' src={
-        photoURL ? photoURL : "https://icon-library.com/images/anonymous-icon/anonymous-icon-0.jpg"} />
-        <b>{displayName ? displayName : "UserWithoutName"}</b>
+      <div className='FriendBox'>
+        <div onClick={() => { PakeistiPokalbi(uid); props.handleClick('ChatRoom') }} >
+          <img alt=':(' className='Profile-imgFR name' src={
+            photoURL ? photoURL : "https://icon-library.com/images/anonymous-icon/anonymous-icon-0.jpg"} />
+          <b>{displayName ? displayName : "UserWithoutName"}</b>
+        </div>
+        <div>
+          <button className='rmvFriend' onClick={() => { RemoveFriend(props.friend) }}><Icon.XCircle /></button>
+        </div>
+      </div>
     </div>
-    <button className='rmvFriend' onClick={() => {RemoveFriend(props.friend)}}>X</button>
-    </div>
-
-    
   )
 }
 
@@ -300,10 +298,11 @@ const RemoveFriend = (friend) => {
   const friendsRefKitam = firestore.collection('friends' + friend.uid).doc(auth.currentUser.uid);
   friendsRefSau.delete();
   friendsRefKitam.delete();
+  pokalbis = "messages";
 }
 
 //Prideti drauga
-const AddFriend = (userId, uid, displayName,photoURL) => {
+const AddFriend = (userId, uid, displayName, photoURL) => {
   const friendsRefSau = firestore.collection('friends' + userId).doc(uid);
   const friendsRefKitam = firestore.collection('friends' + uid).doc(userId);
   friendsRefSau.set({
@@ -315,22 +314,20 @@ const AddFriend = (userId, uid, displayName,photoURL) => {
   friendsRefKitam.set({
     uid: auth.currentUser.uid,
     displayName: auth.currentUser.displayName,
-    photoURL:auth.currentUser.photoURL,
+    photoURL: auth.currentUser.photoURL,
     createdAt: firebase.firestore.FieldValue.serverTimestamp(),
   })
 }
 
 //Pakeisti pokalbi
-function PakeistiPokalbi(draugoId){
-  if (draugoId.localeCompare(auth.currentUser.uid)>0){
-    pokalbis = auth.currentUser.uid+"messages"+draugoId;
+function PakeistiPokalbi(draugoId) {
+  if (draugoId.localeCompare(auth.currentUser.uid) > 0) {
+    pokalbis = auth.currentUser.uid + "messages" + draugoId;
   }
-  else{
-    pokalbis = draugoId+"messages"+auth.currentUser.uid;
+  else {
+    pokalbis = draugoId + "messages" + auth.currentUser.uid;
   }
 }
-
-
 
 
 export default App;
